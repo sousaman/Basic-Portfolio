93727028bc481ba66ab16a1286fd59b6d86abf69
James Henley <james.l.henley@gmail.com>
2018-02-24 13:32:01 -0500

    adding logic to let people craft emails from the contact page

---
+++ assets/js/email.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    var email = "james.l.henley@gmail.com";
+    var subject = "";
+    var body = "";
+    var form = document.querySelector("#form");
+    form.addEventListener("submit", function(event) {
+        subject = "Portfolio - " + form.elements.exampleInputName1.value;
+        body = form.elements.exampleFormControlTextarea1.value; 
+        console.log('mailto:' + email +'?subject=' + subject + '&body=' + body);
+        window.open('mailto:' + email +'?subject=' + subject + '&body=' + body);
+        event.preventDefault();
+      });
+
+    /* $(".submit").on("click", function() {
+        subject = "Portfolio - " + $("#name").val();
+        body = $("#message").val();
+        console.log('mailto:' + email +'?subject=' + subject + '&body=' + body);
+        window.open('mailto:' + email +'?subject=' + subject + '&body=' + body);
+    }) */
+})
+
